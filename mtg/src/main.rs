ba13ee1adb1ca7a3e540c14ebe9ac107b7fbc705
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-10-02 22:11:53 +0200

    Wrong commit add graveyard to player

--- mtg/src/main.rs
+++ mtg/src/main.rs
@@ -8,65 +8,84 @@ fn main() {
     Builder::from_env(Env::default().default_filter_or("debug"))
         .target(Target::Stdout)
         .init();
-    let creature_1 = Creature {
-        name: "Rat".to_string(),
-        power: 2,
-        toughness: 2,
-    };
-    let creature_2 = Creature {
-        name: "Small Rat".to_string(),
-        power: 2,
-        toughness: 1,
-    };
-    let user = Player {
-        name: "Carlos".to_string(),
-        creatures: vec![creature_1],
-    };
-    let cpu = Player {
-        name: "CPU".to_string(),
-        creatures: vec![creature_2],
-    };
+    let creature_1 = Creature::new("Rat", 2, 2);
+    let creature_2 = Creature::new("Small Rat", 2, 1);
+    let user = Player::<Card>::new("Carlos");
+    let cpu = Player::new("CPU");
     log::info!("Init game");
-    loop {
-        let player = user.clone();
-        log::info!("Turn of the player {}", player.name);
-        log::info!("Init combat");
-        log::info!("Do you want to attack? [y/N]");
-        let mut answer = String::new();
-        io::stdin()
-            .read_line(&mut answer)
-            .expect("Failed to read line");
-        answer = answer.trim().to_string();
-        let is_player_attacking = answer.to_lowercase() == "y".to_string();
-        if !is_player_attacking {
-            continue;
-        }
-        let attacker = user.creatures[0].clone();
-        let blocker = cpu.creatures[0].clone();
-        log::info!("{} vs {}", attacker, blocker);
-        let new_attacker = get_creature_after_combat(&blocker, &attacker);
-        let new_blocker = get_creature_after_combat(&attacker, &blocker);
-        log::debug!("Result: {} and {}", new_attacker, new_blocker);
-    }
+    //loop {
+    //    let player = user.clone();
+    //    log::info!("Turn of the player {}", player.name);
+    //    log::info!("Init combat");
+    //    log::info!("Do you want to attack? [y/N]");
+    //    let mut answer = String::new();
+    //    io::stdin()
+    //        .read_line(&mut answer)
+    //        .expect("Failed to read line");
+    //    answer = answer.trim().to_string();
+    //    let is_player_attacking = answer.to_lowercase() == "y".to_string();
+    //    if !is_player_attacking {
+    //        continue;
+    //    }
+    //    let attacker = user.creatures[0].clone();
+    //    let blocker = cpu.creatures[0].clone();
+    //    log::info!("{} vs {}", attacker, blocker);
+    //    let new_attacker = get_creature_after_combat(&blocker, &attacker);
+    //    let new_blocker = get_creature_after_combat(&attacker, &blocker);
+    //    log::debug!("Result: {} and {}", new_attacker, new_blocker);
+    //}
 }
+
 
 #[derive(Clone, Debug)]
 struct Creature {
-    name: String,
+    name: &'static str,
     power: i8,
     toughness: i8,
 }
 
-#[derive(Clone)]
-struct Player {
-    name: String,
-    creatures: Vec<Creature>,
+trait Card {
+    fn name(&self) -> &'static str;
+}
+
+impl Creature {
+    fn new(name: &'static str, power: i8, toughness: i8) -> Self {
+        Creature { name, power, toughness}
+    }
+}
+
+impl Card for Creature {
+    fn name(&self) -> &'static str {
+        self.name
+    }
 }
 
 impl fmt::Display for Creature {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}({}, {})", self.name, self.power, self.toughness)
     }
+}
+
+#[derive(Clone)]
+struct Player<T: Card + Sized>{
+    name: &'static str,
+    creatures: Vec<Creature>,
+    graveyard: Vec<T>,
+}
+
+impl<T: Card + Sized> Player<T> {
+    fn new(name: &'static str) -> Player<T> {
+        Player { name, creatures: Vec::new(), graveyard: Vec::new() }
+    }
+
+    fn add_creature(&self, creature: Creature) {
+        self.creatures.push(creature);
+    }
+
+    fn add_graveyard(&self, card: T) {
+        self.graveyard.push(card);
+    }
+
 }
 
 fn get_creature_after_combat(attacker: &Creature, blocker: &Creature) -> Creature {
